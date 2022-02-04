3cad2077229cf252a7d160138742f5c585cc707e
niromru <emailr47@gmail.com>
2022-02-04 18:06:33 +0545

    creates a new window

--- keylogger_gui.py
+++ keylogger_gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import sys
 
 class MainWindow:
   def __init__(self, root, title, geometry, message):
@@ -6,6 +7,20 @@ class MainWindow:
     self.root.title(title)
     self.root.geometry(geometry)
     Label(self.root, text=message, font=("Helvetica", 16)).pack(pady=20)
+    button1 = Button(self.root, text="logs", command=self.logs)
+    button1.pack()
+    self.root.mainloop()
+
+  def logs(self):
+    self.new_win =  Toplevel(self.root)
+    self.new_win = Showlogs(self.new_win, "logs", "1280x720+360+210", "Logs are here")
+
+class Showlogs:
+  def __init__(self, root, title, geometry, message):
+    self.root = root
+    self.root.title(title)
+    self.root.geometry(geometry)
+    Label(self.root, text=message, font=("Helvetica", 16)).pack(pady=2)
     self.root.mainloop()
 
 def main():
